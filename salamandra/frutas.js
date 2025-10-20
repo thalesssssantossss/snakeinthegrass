69e62e84c835c3d82f5e0b3f4e2af87648193091
thalesssssantossss <thales.ssssantossss@gmail.com>
2025-10-20 19:28:29 -0300

    Fix getFrutas class definition and remove duplicates
    Refactor getFrutas class to properly define constructor and methods, and remove duplicate code.

--- salamandra/frutas.js
+++ salamandra/frutas.js
@@ -1,1 +1,45 @@
+class getFrutas {
+    frutas = ['manzana', 'banana', 'cereza', 'durazno', 'uva'];
+    frutas;
+}
+    constructor(); {
+        this.frutas = this.frutas;
+    }
+    getFrutas(); {
+        return this.frutas;
+    }
 
+module.exports = getFrutas;
+
+let frutasInstance = new getFrutas();
+console.log(frutasInstance.getFrutas());
+
+Math.random();toString(36).substring(2, 15);
+class getFrutas {
+    frutas = ['manzana', 'banana', 'cereza', 'durazno', 'uva'];
+    frutas;
+}
+    constructor(); {
+        this.frutas = this.frutas;
+    }
+    getFrutas(); {
+        return this.frutas;
+    }
+module.exports = getFrutas;
+
+Math.floor(Math.random() * (max - min + 1)) + min;
+Math.floor(Math.random() * array.length);
+const min = 1;
+const max = 100;
+const numeroSorteado = Math.floor(Math.random() * (max - min + 1)) + min;
+class getFrutas {
+    frutas = ['manzana', 'banana', 'cereza', 'durazno', 'uva'];
+    frutas;
+}
+    constructor(); {
+        this.frutas = this.frutas;
+    }
+    getFrutas(); {
+        return this.frutas;
+    }   
+module.exports = getFrutas;
